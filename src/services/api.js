3bc76996bf0aa8aafff32577a6695be0c71b0e03
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-01-20 14:37:57 -0300

    Consertando a rota de base

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 }); */
 
 const api = axios.create({
-  baseURL: "https://semana-omnistack-10.herokuapp.com/devs"
+  baseURL: "https://semana-omnistack-10.herokuapp.com"
 });
 
 export default api;
